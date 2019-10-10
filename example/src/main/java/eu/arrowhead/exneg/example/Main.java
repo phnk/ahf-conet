b5dddd0b2b4da9eeb7991e900af6b39ab3680d23
Emanuel Palm <emanuel.palm@ltu.se>
2019-10-10 17:02:30 +0200

    Change to new AHF core client library

---
+++ example/src/main/java/eu/arrowhead/exneg/example/Main.java
@@ -0,0 +1,15 @@
+package eu.arrowhead.exneg.example;
+
+import eu.arrowhead.client.library.ArrowheadService;
+import eu.arrowhead.common.dto.shared.ServiceRegistryRequestDTO;
+
+public class Main {
+    public void main(final String[] args) {
+        final var service = new ArrowheadService();
+
+        final var req = new ServiceRegistryRequestDTO();
+        req.setServiceUri("/test/test");
+        final var res = service.registerServiceToServiceRegistry(req);
+        System.out.println("Bye!");
+    }
+}
