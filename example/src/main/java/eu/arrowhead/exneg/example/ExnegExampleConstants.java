23554feb14986e3db7608b72cc46b504a12d1455
Emanuel Palm <emanuel.palm@ltu.se>
2019-10-11 11:23:09 +0200

    Work on stuff

---
+++ example/src/main/java/eu/arrowhead/exneg/example/ExnegExampleConstants.java
@@ -0,0 +1,12 @@
+package eu.arrowhead.exneg.example;
+
+public final class ExnegExampleConstants {
+    public static final String BASE_PACKAGE = "eu.arrowhead.exneg";
+    public static final String INTERFACE_SECURE = "HTTPS-SECURE-JSON";
+    public static final String INTERFACE_INSECURE = "HTTP-INSECURE-JSON";
+    public static final String HTTP_METHOD = "http-method";
+
+    private ExnegExampleConstants() {
+        throw new UnsupportedOperationException();
+    }
+}
